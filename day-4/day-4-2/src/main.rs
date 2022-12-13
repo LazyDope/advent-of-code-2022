85a63a3fcf7565c5417981079e7e5d8043e2c9a3
LazyDope <mydj555@gmail.com>
2022-12-12 23:19:05 -0500

    finish day 4, start day 5

--- day-4/day-4-2/src/main.rs
+++ day-4/day-4-2/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input").unwrap();
+    let size = input
+        .lines()
+        .map(|lines| {
+            lines
+                .split(",")
+                .map(|range| {
+                    range
+                        .split("-")
+                        .map(|num| num.parse::<u32>().unwrap())
+                        .collect::<Vec<u32>>()
+                })
+                .collect::<Vec<Vec<u32>>>()
+        })
+        .filter(|pairs| {
+            (pairs[0][0] <= pairs[1][0] && pairs[1][0] <= pairs[0][1])
+                || (pairs[0][0] <= pairs[1][1] && pairs[1][1] <= pairs[0][1])
+                || (pairs[1][0] <= pairs[0][0] && pairs[0][1] <= pairs[1][1])
+        })
+        .count();
+
+    println!("{}", size);
 }
---
+++ day-5/day-5-1/src/main.rs
@@ -0,0 +1,6 @@
+use std::fs;
+
+fn main() {
+    let cargo = fs::read_to_string("input1").unwrap();
+    let instructions = fs::read_to_string("input2").unwrap();
+}
